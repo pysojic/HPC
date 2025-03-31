78f57c33f60229cca75161e2274c04b3d784cd6e
pysojic <pysojic@uchicago.edu>
2025-03-31 15:33:37 -0500

    Added AssignmentD instructions

--- Assignments/AssignmentD/main.cpp
+++ Assignments/AssignmentD/main.cpp
@@ -1,3 +1,16 @@
+/*
+▶Calculating the value of π using Monte Carlo involves the following steps:
+1. Assume the circle is centered at coordinates (0, 0).
+2. Generate N random points with coordinates (x, y) where x and y are independently drawn from a 
+   uniform distribution over the interval [-1, 1].
+3. Determine if each point lies inside the circle or not.
+▶ The value of π can be estimated using the simulation results as follows.
+ρ = M/N = π/4
+where, N = total number of points generated and M = number of random points inside the circle.
+▶ Write a program to calculate the value of π using information above, for N = 100, 1000 and 10000. 
+▶Write the results to the standard output.
+*/
+
 #include <iostream>
 #include <random>
 #include <format>
